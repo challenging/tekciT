1cafa55f97ce1ac2d8204d05101c3e403dbea1df
Rungchi Chen <rungchi_chen@migocorp.com>
2014-10-28 09:45:15 +0800

    [Code Info] - Remove the unnecessary code

--- tickets/spiders/csair.py
+++ tickets/spiders/csair.py
@@ -49,37 +49,6 @@ class CsairSpider(scrapy.Spider):
             except UnexpectedAlertPresentException as e:
                 tries -= 1
 
-    '''
-0 去程2014年10月04日广州 - 墨尔本
-1 头等舱
-2 公务舱
-3 高端经济舱
-4 经济舱
-5 CZ343
-6 直达 空客333 09:00  广州
-7 20:30  墨尔本
-8 经济舱来回程票价
-9 ¥9610
-10 含税总价 ¥ 11672
-11 查看票价使用条件
-12 选择航班
-13 显示详细信息
-14 去程2014年10月04日广州 - 墨尔本
-15 头等舱
-16 公务舱
-17 高端经济舱
-18 经济舱
-19 CZ321
-20 直达 空客333 21:00  广州
-21 09:40  墨尔本
-22 10月05日 星期日
-23 经济舱来回程票价
-24 ¥10920
-25 含税总价 ¥ 12982
-26 查看票价使用条件
-27 选择航班
-28 显示详细信息
-    '''
     def flight(self, infos):
         lines = []
 
@@ -116,12 +85,16 @@ class CsairSpider(scrapy.Spider):
             yield ticket
 
     def parse(self, response):
+        count_fail = 0
         for plusDate in range(self.dateStart, self.dateEnd+1):
+            if count_fail >= 7:
+                break
+
             flyingDate = datetime.date.today() + datetime.timedelta(days=int(plusDate))
             backDate = datetime.date.today() + datetime.timedelta(days=int(plusDate)+1)
 
             self.browser.get(CsairSpider.search_url)
-            time.sleep(3)
+            time.sleep(5)
 
             self.js("document.getElementsByName(\"segtype_1\")[1].click();")
 
@@ -141,31 +114,13 @@ class CsairSpider(scrapy.Spider):
                 isCompleted = ui.WebDriverWait(browser, 10).until(lambda browser: len(browser.find_elements_by_id("flight_info")) > 0 and len(browser.find_elements_by_id("flight_info")[0].text) > 0)
                 if isCompleted:
                     infos = browser.find_elements_by_id("flight_info")[0].text.split("\n")
-                    #for idx in range(0, len(infos)):
-                    #    print idx, infos[idx]
 
-                    '''
-                    n = 14
-                    for idx in range(0, len(infos)/n):
-                        ticket = CsairTicket()
-                        ticket["company"] = self.name
-                        ticket["flight"] = infos[idx*n + 5]
-                        ticket["fromCity"] = self.fromCityCode
-                        ticket["toCity"] = self.toCityCode
-
-                        for ii in range(0, n-1):
-                            if u"含税总价 ¥" in infos[idx*n + ii]:
-                                ticket["price"] = "".join(infos[idx*n + ii].split(" ")[1:])
-
-                        ticket["info"] = "\n".join(infos)
-                        ticket["flyingDate"] = infos[idx*n]
-                        ticket["datetimeStart"] = infos[idx*n + 6].split(" ")[2]
-                        ticket["datetimeEnd"] = infos[idx*n + 7].split(" ")[0]
-
-                        yield ticket
-                    '''
                     for ticket in self.flight(infos):
                         yield ticket
+
+                    count_fail = -1
+
+                count_fail += 1
             except exceptions.IndexError as e:
                 pass
             except TimeoutException as e:
