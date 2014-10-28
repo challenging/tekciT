a1d149e9e133d4db2846c795d0bbd7d8693efce8
Rungchi Chen <rungchi_chen@migocorp.com>
2014-10-28 10:24:51 +0800

    [Fix] - Not Found the configuration files

--- Flights/scrapy.sh
+++ Flights/scrapy.sh
@@ -4,28 +4,29 @@ path_ticket=$(dirname $0)
 
 run(){
     local scrapy=$1
-    local from=$2
-    local to=$3
+    local to=$2
 
     for city in $(ls ${to}/toCity*cfg);
     do
-        echo ${scrapy} ${from} ${city}
+        to_cfg=$(basename ${city})
+        
+        ${scrapy} fromCity.cfg ${file}
     done
 }
 
 # China Airline
-run ${path_ticket}/ChinaAirline/chinaairline.sh ${path_ticket}/ChinaAirline/fromCity.cfg ${path_ticket}/ChinaAirline &
+run ${path_ticket}/ChinaAirline/chinaairline.sh ${path_ticket}/ChinaAirline &
 
 # 亞洲航空
-run ${path_ticket}/AirAsia/asiaair.sh ${path_ticket}/AirAsia/fromCity.cfg ${path_ticket}/AirAsia &
+run ${path_ticket}/AirAsia/asiaair.sh ${path_ticket}/AirAsia &
 
 # 酷航
-run ${path_ticket}/Flyscoot/flyscoot.sh ${path_ticket}/Flyscoot/fromCity.cfg ${path_ticket}/Flyscoot &
+run ${path_ticket}/Flyscoot/flyscoot.sh ${path_ticket}/Flyscoot &
 
 # 捷星航空
-run ${path_ticket}/JetStar/jetstar.sh ${path_ticket}/JetStar/fromCity.cfg ${path_ticket}/JetStar &
+run ${path_ticket}/JetStar/jetstar.sh ${path_ticket}/JetStar &
 
 # CebuPacificAir
-run ${path_ticket}/CebuPacificAir/cebupacificair.sh ${path_ticket}/CebuPacificAir/fromCity.cfg ${path_ticket}/CebuPacificAir &
+run ${path_ticket}/CebuPacificAir/cebupacificair.sh ${path_ticket}/CebuPacificAir &
 
 wait
--- Flights/selenium.sh
+++ Flights/selenium.sh
@@ -4,34 +4,34 @@ ws_path=$(dirname $0)
 
 run(){
     local scrapy=$1
-    local from=$2
-    local to=$3
+    local to=$2
 
     for city in $(ls ${to}/toCity*.cfg);
     do
-        echo ${scrapy} ${from} ${city}
+        city=$(basename ${city})
+        ${scrapy} fromCity.cfg ${city}
     done
 }
 
 # 樂桃航空
-run ${ws_path}/Peach/peach.sh ${ws_path}/Peach/fromCity.cfg ${ws_path}/Peach &
+run ${ws_path}/Peach/peach.sh ${ws_path}/Peach &
 
 # 長榮航空
-run ${ws_path}/EvaAirline/evaairline.sh ${ws_path}/EvaAirline/fromCity.cfg ${ws_path}/EvaAirline &
+#run ${ws_path}/EvaAirline/evaairline.sh ${ws_path}/EvaAirline &
 
 # 中國航空
-run ${ws_path}/ChinaEastern/chinaeastern.sh ${ws_path}/ChinaEastern/fromCity.cfg ${ws_path}/ChinaEastern &
+run ${ws_path}/ChinaEastern/chinaeastern.sh ${ws_path}/ChinaEastern &
 
 # AirBusan
-run ${ws_path}/AirBusan/airbusan.sh ${ws_path}/AirBusan/fromCity.cfg ${ws_path}/AirBusan &
+run ${ws_path}/AirBusan/airbusan.sh ${ws_path}/AirBusan &
 
 # 香草航空 - VanillaAir
-run ${ws_path}/VanillaAir/vanillaair.sh ${ws_path}/VanillaAir/fromCity.cfg ${ws_path}/VanillaAir &
+run ${ws_path}/VanillaAir/vanillaair.sh ${ws_path}/VanillaAir &
 
 # 虎航
-run ${ws_path}/Flight/TigerAir/tigerair.sh ${ws_path}/TigerAir/fromCity.cfg ${ws_path}/TigerAir &
+run ${ws_path}/Flight/TigerAir/tigerair.sh ${ws_path}/TigerAir &
 
 # 南方航空
-run ${ws_path}/Flight/CsAir/csair.sh ${ws_path}/CsAir/fromCity.cfg ${ws_path}/CsAir &
+run ${ws_path}/Flight/CsAir/csair.sh ${ws_path}/CsAir &
 
 wait
